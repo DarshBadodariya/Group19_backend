ad343dc2b3e840af88ad0e8d48eb4611031d4188
aayushh11 <aayushp11@hotmail.com>
2021-05-06 12:14:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,26 +5,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const fileUpload = require('express-fileupload')
-const path=require('path')
-
 const User = require("./models/user");
 
-app.use(fileUpload({
-    useTempFiles : true,
-    //tempFileDir : '/tmp/'
-}));
-/*
-app.use((req, res, next) => {
-    console.log("user :  " + req.user + " ===== files : " + req.files);
-    next();
-})
-*/
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/userprof");
 const productRoutes = require("./routes/product");
-const uploadRoutes=require("./routes/upload");
+const userfav = require("./routes/userfav")
 
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true,
@@ -39,48 +26,12 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-app.use("/api",uploadRoutes);
+
 app.use("/api", authRoutes);
-app.use("/api", userRoutes);
+app.use("/api/userprof", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api",userfav)
 
-<<<<<<< Updated upstream
-var collection = mongoose.DATABASE("sample_mflix").collection("movies");
-
-app.get("api/products/search", async (req, res) => {
-    try {
-        let result = await collection.aggregate([
-            {
-                "$search": {
-                    "text": {
-                        "query": `${req.body.search}`,
-                        "path": "plot",
-                        "fuzzy": {
-                            "maxEdits": 2
-                        }
-                    },
-                    "highlight": {
-                        "path": "plot"
-                    }
-                }
-            },
-            //{
-            //    "$addFields": {
-            //        "highlights": {
-            //            "$meta": "searchHighlights"
-            //        }
-            //    }
-            //}
-        ]).toArray();
-        res.json(result);
-    } catch (e) {
-        res.status(500).send({ message: e.message });
-    }
-=======
-app.get('/',(req,res)=>{
-    res.send("HEllo welcome to the server");
->>>>>>> Stashed changes
-});
 
 const port = process.env.PORT || 4001;
 //Starting a server 
