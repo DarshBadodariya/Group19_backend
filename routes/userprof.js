2e90c2c2b69020a9179eb82bb725fd054154ea9f
Moradiya-Shivam <78947904+Moradiya-Shivam@users.noreply.github.com>
2021-05-08 20:16:06 +0530

    updated userprof routs
    Added comments and written the use of each post get and put request in the userprof rout

--- routes/userprof.js
+++ routes/userprof.js
@@ -5,7 +5,7 @@ const products = require('../models/product');
 
 const router = express.Router();
 
-
+// Getting the perticular user profile information using get request
 router.get('/:id', async (req,res) => {
 
     try{
@@ -17,13 +17,13 @@ router.get('/:id', async (req,res) => {
 
 });
 
-
+//This put request will update the user profile information 
 router.put('/:id', /*, issignedin, isretailer,*/ async (req,res) =>{
 
     const user = await users.findById(req.params.id);
 
 
-
+    // if user exist then update the all changes in the databse
     if(user){
 
         const upduser = req.body;
@@ -48,7 +48,7 @@ router.put('/:id', /*, issignedin, isretailer,*/ async (req,res) =>{
     }
 });
 
-
+//delete the user 
 router.delete('/:id', /*, issignedin, isretailer,*/ async (req,res) =>{
     
     const deluser = await users.findById(req.params.id);
@@ -62,20 +62,23 @@ router.delete('/:id', /*, issignedin, isretailer,*/ async (req,res) =>{
     }
 });
 
-
-router.get('/addtocart/:id/:productId',  async(req, res) => {
+// This post request wiol take the userid and productid 
+// and add the selected item to the users cart
+router.post('/addtocart/:id/:productId',  async(req, res) => {
 
     const userInfo = await users.findOne({ _id: req.params.id});
     const productInfo = await products.findOne({ _id: req.params.productId});
     
         let duplicate = false;
+        //checking for duplicate item
         userInfo.cart.forEach((item) =>{
             if (item.id == req.params.productId) {
                 duplicate = true;
             }
         })
         
-       
+       // if duplicate then increase the quantity by 1
+       // else add the item to the cart with initial quntity 1
         if (duplicate) {
             users.findOneAndUpdate(
                 { _id: req.params.id, "cart.id": req.params.productId },
@@ -113,7 +116,32 @@ router.get('/addtocart/:id/:productId',  async(req, res) => {
     
 });
 
-router.get('/removefromCart/:id/:productId',  async(req, res) => {
+// THis rout will fetch the users cart information 
+//if cart is emty then msg 
+//else fetch all the product
+router.get('/mycart/:id', async(req,res) => {
+
+    const userInfo = await users.findOne({ _id: req.params.id});
+    if(userInfo){
+        const mycart = userInfo.cart;
+        if(mycart!=null){
+            return res.json(mycart);
+            //console.log(mycart);
+        }
+        else{
+            return res.json({ msg: 'cart is empty' });
+            //console.log({msg: 'cart is empty'});
+        }
+    }
+    else{
+        return res.json({msg: 'user not found'});
+        //console.log({msg: "cart is empty"});
+    }
+});
+
+// Remove the perticular product from the cart
+//return the cart info if succesfull 
+router.post('/removefromCart/:id/:productId',  async(req, res) => {
     
     users.findOneAndUpdate(
         { _id: req.params.id },
@@ -130,9 +158,8 @@ router.get('/removefromCart/:id/:productId',  async(req, res) => {
     )
 })
 
-//606dd40ab38a536484f062db
-//6066f41789350f21e06b0440
-
+// This request will brings all the items into purchases and then 
+//empty the users cart
 router.get('/purchase/:id', async(req, res) => {
 
     const userInfo = await users.findOne({ _id: req.params.id});
@@ -165,6 +192,8 @@ router.get('/purchase/:id', async(req, res) => {
             )
 });
 
+
+//This request will reset the all past orders
 router.get('/resetpurchase/:id',  async(req, res) => {
 
         users.findOneAndUpdate(
